74ccaf7cdaa6811b65cea3eebf7c1b432582935d
janith-rathanyaka <mnjrathnayaka97@yahoo.com>
2025-01-02 08:10:28 +0530

    add

--- backend/libs/shared/src/schemas/user.schema.ts
+++ backend/libs/shared/src/schemas/user.schema.ts
@@ -3,12 +3,13 @@ import { User } from '../interface/user.interface';
 
 // Extend User with Mongoose Document
 export interface UserDocument extends Omit<User, 'id'>, Document {
-    _id: string; 
-  }
+  _id: string;
+}
 
 export const UserSchema = new Schema<UserDocument>({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
